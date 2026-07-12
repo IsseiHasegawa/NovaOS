ad145cf4dcb8cfdc09f3d40ec88d63a4f0506c8e
Issei Hasegawa <149334273+IsseiHasegawa@users.noreply.github.com>
2026-07-12 15:25:40 -0400

    Implement uart

--- kernel/uart.c
+++ kernel/uart.c
@@ -1,10 +1,23 @@
-#ifndef UART_H
-#define UART_H
+#include "uart.h"
 
-#include <stdint.h>
+#define UARTO 0x10000000L
+#define UART_THR (UARTO + 0x00) /* transmit holding register */
 
-void uart_putc(char c);
-void uart_puts(const char *s);
-void uart_puthex(uint64_t x);
+void uart_putc(char c) {
+    volatile uint8_t *thr = (volatile uint8_t *)UART_THR;
+    *thr = (uint8_t)c;
+}
 
-#endif
+void uart_puts(const char *s) {
+    while (*s) {
+        uart_puts(*s++);
+    }
+}
+
+void uart_puthex(uint64_t v) {
+    uart_puts("0x");
+    for (int i = 60; i >= 0; i -= 4) {
+        int d = (v >> i) & 0xf;
+        uart_putc(d < 10 ? '0' + d : 'a' + (d - 10));
+    }
+}
